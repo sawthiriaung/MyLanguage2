e8d41e0572c12c3235447c1413f1d081325839bc
XANN012\User <sawthiriaung.93@gmail.com>
2019-06-20 18:07:33 +0630

    second commit

---
+++ Myviewpager/app/src/main/java/com/nmh/speaktotext/myviewpager/InfiniteAdapter.java
@@ -0,0 +1,61 @@
+package com.nmh.speaktotext.myviewpager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+class InfiniteAdapter extends PagerAdapter {
+    private Context context;
+    private List<Integer> color;
+    private List<String> colorName;
+
+    public InfiniteAdapter(Context context, List<Integer> color, List<String> colorName) {
+        this.context = context;
+        this.color = color;
+        this.colorName = colorName;
+    }
+
+    @Override
+    public int getCount() {
+        return color.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_slider, null);
+
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.linearLayout);
+
+        textView.setText(colorName.get(position));
+        linearLayout.setBackgroundColor(color.get(position));
+
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
---
+++ Myviewpager/app/src/main/java/com/nmh/speaktotext/myviewpager/MainActivity.java
@@ -0,0 +1,77 @@
+package com.nmh.speaktotext.myviewpager;
+
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+//import com.viewpagerindicator.CirclePageIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    InfiniteAdapter adapter;
+    TabLayout indicator;
+
+    List<Integer> color;
+    List<String> colorName;
+//    int[] image={R.drawable.slide1,R.drawable.slide2,R.drawable.slide3};
+//    private Handler handler = new Handler();
+//    private ArrayList<Integer>  ImgArray = new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide);
+
+        viewPager = findViewById(R.id.viewPager);
+        indicator= findViewById (R.id.indicator);
+
+        color = new ArrayList<>();
+        color.add(Color.RED);
+        color.add(Color.GREEN);
+        color.add(Color.BLUE);
+
+        colorName = new ArrayList<>();
+        colorName.add("RED");
+        colorName.add("GREEN");
+        colorName.add("BLUE");
+
+        viewPager.setAdapter(new InfiniteAdapter(this, color, colorName));
+        indicator.setupWithViewPager(viewPager, true);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
+    }
+
+    private class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem() < color.size() - 1) {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
+
+}
